9ed9bafeaa81782bce9d2fed7285d86b15e63fff
Macjhony Vargas <mvargas@darient.com>
2023-06-09 13:39:30 -0400

    STATICOS

--- darient_credit/settings.py
+++ darient_credit/settings.py
@@ -131,6 +131,11 @@ USE_TZ = True
 
 #Ruta para poder utilizar archivos estaticos, como css, bootstrap
 STATIC_URL = '/static/'
+STATIC_FILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
+
 if not DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
